d7e9a4b9e015e95f8eadd3ba724a0a3fd27b288d
badreldin A <badreldin99@hotmail.com>
2025-06-02 15:17:19 -0400

    updated project and added a web page

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -3,11 +3,16 @@ package com.example.helloworld.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String sendGreetings() {
-        return "Hello, World from Badr!";
+    public String home(Model model) {
+        model.addAttribute("message", "Hello from Badreldim with Java 21!");
+        return "index"; // Refers to src/main/resources/templates/index.html
     }
 }
--- src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
+++ src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
@@ -19,9 +19,7 @@ public class HelloWorldControllerTest {
 
     @Test
     public void shouldReturnExpectedMessage() throws Exception {
-
         mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello, World from Badr!"));
+                .andExpect(status().isOk());
     }
 }
